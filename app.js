d7ec76518bfb43df52dadac929454a1b2ac61b92
Kimiko Muwanguzi <kimikomu@gmail.com>
2017-10-15 19:00:19 -0400

    Add new route. Add port location message to console.

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, resp) => {
-	res.send('I love Treehouse!!');
+app.get('/', (req, res) => {
+	res.send('<h1>I love Treehouse!</h1>');
 });
 
-app.listen(3000);
+app.get('/hello', (req, res) => {
+	res.send('<h1>Hello JavaScript developer!</h1>');
+});
+
+app.listen(3000, () => {
+	console.log('The application is running on localhost 3000.');
+});

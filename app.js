2b0ec22086043801bb2b9f84df7fb8e0d61656f9
Kimiko Muwanguzi <kimikomu@gmail.com>
2017-10-16 12:08:18 -0400

    Move welcome to the index page and add goodbye button

--- app.js
+++ app.js
@@ -10,20 +10,35 @@ app.use(cookieParser());
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-	res.render('index');
+	const name = req.cookies.username;
+	if (name) {
+		res.render('index', { name });
+	} else {
+		res.redirect('/hello');
+	}
 });
 
 app.get('/cards', (req, res) => {
 	res.render('card', { prompt: "Who is buried in Grant's tomb?", hint: "Think about who's tomb it is." });
 });
 
-app.get('/hello', (req, res) => {
-	res.render('hello', { name: req.cookies.username });
+app.get('/hello', (req, res) => {	
+	const name = req.cookies.username;
+	if (name) {
+		res.redirect('/');
+	} else {
+		res.render('hello');
+	}
 });
 
 app.post('/hello', (req, res) => {
 	res.cookie('username', req.body.username);
-	res.render('hello', { name: req.body.username });
+	res.redirect('/');
+});
+
+app.post('/goodbye', (req, res) => {
+	res.clearCookie('username');
+	res.redirect('/hello');
 });
 
 app.listen(3000, () => {

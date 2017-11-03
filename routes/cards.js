9568378574df35401d21800deaa8f86f790616cf
Kimiko Muwanguzi <kimikomu@gmail.com>
2017-11-02 20:18:50 -0400

    Show question/answer side of card. Card chosen randomly

--- routes/cards.js
+++ routes/cards.js
@@ -6,18 +6,36 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
+// get random card number
+router.get('/', (req, res) => {
+	 const numberOfCards = cards.length;
+	 const flashcardId = Math.floor(Math.random() * numberOfCards);
+	 res.redirect(`/cards/${flashcardId}`);
+});
+
+
 // collect card info
 router.get('/:id', (req, res) => {
 	const { side } = req.query;
 	const { id } = req.params;
+
+	// go to question side if a side is not provided
+	if(!side) {
+		res.redirect(`/cards/${id}?side=question`);		
+	}
+
 	const text = cards[id][side];
 	const { hint } = cards[id];
-
-	const templateData = { text };
+	const templateData = { id, text };
 
 	// chose the side with the question or answer
-	if (side === 'question') {
+	if(side === 'question') {
 		templateData.hint = hint;
+		templateData.sideToShow = 'answer';
+		templateData.sideToShowDisplay = 'Answer';
+	} else if(side === 'answer') {
+		templateData.sideToShow = 'question';
+		templateData.sideToShowDisplay = 'Question';
 	}
 
 	res.render('card', templateData);

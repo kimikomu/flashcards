d904aad18fdea38cbda019ea95f71e51c5848760
Kimiko Muwanguzi <kimikomu@gmail.com>
2017-11-03 11:59:03 -0400

    Attach designer front end files for a better user experience

--- routes/cards.js
+++ routes/cards.js
@@ -1,45 +1,39 @@
 const express = require('express');
 const router = express.Router();
-
-// const data = require('../data/flashcardData.json').data;
-// const cards = data.cards;
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
 // get random card number
 router.get('/', (req, res) => {
-	 const numberOfCards = cards.length;
-	 const flashcardId = Math.floor(Math.random() * numberOfCards);
-	 res.redirect(`/cards/${flashcardId}`);
+  const numberOfCards = cards.length;
+  const flashcardId = Math.floor(Math.random() * numberOfCards);
+  res.redirect(`/cards/${flashcardId}`);
 });
-
 
 // collect card info
 router.get('/:id', (req, res) => {
-	const { side } = req.query;
-	const { id } = req.params;
+    const { side } = req.query;
+    const { id } = req.params;
 
-	// go to question side if a side is not provided
-	if(!side) {
-		return res.redirect(`/cards/${id}?side=question`);		
-	}
-	const name = req.cookies.username;
-	const text = cards[id][side];
-	const { hint } = cards[id];
+    	// go to question side if a side is not provided
+    if(!side) {
+        return res.redirect(`/cards/${id}?side=question`);
+    }
+    const name = req.cookies.username;
+    const text = cards[id][side];
+    const { hint } = cards[id];
+    
+    const templateData = { id, text, name, side };
 
-	const templateData = { id, text, name };
+    	// chose the side with the question or answer
+    if(side === 'question') {
+      templateData.hint = hint;
+      templateData.sideToShow = 'answer';
+    } else if(side === 'answer') {
+      templateData.sideToShow = 'question';
+    }
 
-	// chose the side with the question or answer
-	if(side === 'question') {
-		templateData.hint = hint;
-		templateData.sideToShow = 'answer';
-		templateData.sideToShowDisplay = 'Answer';
-	} else if(side === 'answer') {
-		templateData.sideToShow = 'question';
-		templateData.sideToShowDisplay = 'Question';
-	}
-
-	res.render('card', templateData);
+    res.render('card', templateData);
 });
 
 module.exports = router;
